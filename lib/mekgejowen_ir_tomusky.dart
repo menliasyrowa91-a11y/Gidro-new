91b492457e26ef9086a76cf1472869363597fd33
Menli <menliasyrowa91@gmail.com>
2026-06-17 16:34:17 +0500

    Add 'çeşme' field to Tapgyr instances
    Updated Tapgyr instances to include 'çeşme' field.

--- lib/mekgejowen_ir_tomusky.dart
+++ lib/mekgejowen_ir_tomusky.dart
@@ -1,12 +1,15 @@
+// mekgejowen_ir_tomusky.dart faýlyňyzyň iň ýokarsyna şu setiri hökman goşuň:
+import 'ekin_model.dart'; 
+
 final Ekin mekgejowenIrTomusky = Ekin(
   ady: "Mekgejöwen (ir tomusda)",
   tapgyrlar: [
-    Tapgyr(ady: "Tagt suwy", normaM3: 1600, gidroModul: 0.882, baha: 47.68, mohlet: "25.III-18.IV", dowamlylykGijeGundiz: 21),
-    Tapgyr(ady: "1-nji ösüş", normaM3: 800, gidroModul: 0.651, baha: 23.84, mohlet: "7.VI-21.VI", dowamlylykGijeGundiz: 16),
-    Tapgyr(ady: "2-nji ösüş", normaM3: 1000, gidroModul: 0.890, baha: 29.80, mohlet: "22.VI-1.VII", dowamlylykGijeGundiz: 13),
-    Tapgyr(ady: "3-nji ösüş", normaM3: 1100, gidroModul: 1.061, baha: 32.78, mohlet: "2.VII-16.VII", dowamlylykGijeGundiz: 12),
-    Tapgyr(ady: "4-nji ösüş", normaM3: 1200, gidroModul: 1.157, baha: 35.76, mohlet: "7.VII-21.VII", dowamlylykGijeGundiz: 12),
-    Tapgyr(ady: "5-nji ösüş", normaM3: 1200, gidroModul: 1.068, baha: 35.76, mohlet: "22.VII-1.VIII", dowamlylykGijeGundiz: 13),
-    Tapgyr(ady: "6-njy ösüş", normaM3: 1100, gidroModul: 0.749, baha: 32.78, mohlet: "2.VIII-16.VIII", dowamlylykGijeGundiz: 17),
+    Tapgyr(ady: "Tagt suwy", normaM3: 1600, gidroModul: 0.882, baha: 47.68, mohlet: "25.III-18.IV", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 21),
+    Tapgyr(ady: "1-nji ösüş", normaM3: 800, gidroModul: 0.651, baha: 23.84, mohlet: "7.VI-21.VI", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 16),
+    Tapgyr(ady: "2-nji ösüş", normaM3: 1000, gidroModul: 0.890, baha: 29.80, mohlet: "22.VI-1.VII", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 13),
+    Tapgyr(ady: "3-nji ösüş", normaM3: 1100, gidroModul: 1.061, baha: 32.78, mohlet: "2.VII-16.VII", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 12),
+    Tapgyr(ady: "4-nji ösüş", normaM3: 1200, gidroModul: 1.157, baha: 35.76, mohlet: "7.VII-21.VII", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 12),
+    Tapgyr(ady: "5-nji ösüş", normaM3: 1200, gidroModul: 1.068, baha: 35.76, mohlet: "22.VII-1.VIII", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 13),
+    Tapgyr(ady: "6-njy ösüş", normaM3: 1100, gidroModul: 0.749, baha: 32.78, mohlet: "2.VIII-16.VIII", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 17),
   ],
 );
