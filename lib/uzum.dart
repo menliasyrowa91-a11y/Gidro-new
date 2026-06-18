322430669583b410c9dcc4d0a3f0442128316260
Menli <menliasyrowa91@gmail.com>
2026-06-18 11:20:43 +0500

    Update uzum.dart

--- lib/uzum.dart
+++ lib/uzum.dart
@@ -4,7 +4,7 @@ import 'ekin_model.dart';
 final Ekin uzumData = Ekin(
   ady: "Üzüm",
   tapgyrlar: [
-    Tapgyr(ady: "Yzgar toplaýyş", normaM3: 1400, gidroModul: 0.523, baha: 41.72, mohlet: "10.XI-10.XII", çeşme: "AnyScanner_06_16_2026-03.jpg", dowamlylykGijeGundiz: 31),
+    Tapgyr(ady: "Yzgar toplaýyş", normaM3: 1400, gidroModul: 0.523, baha: 41.72, mohlet: "10.XI-10.XII", çeşme: "AnyScanner_06_16_2026-03.jpg", dowamlylykGijeGundiz: 11),
     Tapgyr(ady: "Yzgar toplaýyş", normaM3: 1500, gidroModul: 0.482, baha: 44.70, mohlet: "10.I-14.II", çeşme: "AnyScanner_06_16_2026-03.jpg", dowamlylykGijeGundiz: 36),
     Tapgyr(ady: "Yzgar toplaýyş", normaM3: 1500, gidroModul: 0.598, baha: 44.70, mohlet: "15.II-15.III", çeşme: "AnyScanner_06_16_2026-03.jpg", dowamlylykGijeGundiz: 29),
     Tapgyr(ady: "1-nji ösüş", normaM3: 800, gidroModul: 0.298, baha: 23.84, mohlet: "25.V-24.VI", çeşme: "AnyScanner_06_16_2026-03.jpg", dowamlylykGijeGundiz: 31),
