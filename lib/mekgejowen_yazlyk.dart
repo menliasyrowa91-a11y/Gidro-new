ab9320bb36312c9673218c06ceeb532929dcdfc3
Menli <menliasyrowa91@gmail.com>
2026-07-08 11:59:40 +0500

    Update mekgejowen_yazlyk.dart

--- lib/mekgejowen_yazlyk.dart
+++ lib/mekgejowen_yazlyk.dart
@@ -1,19 +1,16 @@
 // lib/mekgejowen_yazlyk.dart
+import 'ekin_model.dart';
 
-// 1. Ilki bilen modeli import edýäris
-import 'ekin_model.dart'; 
-
-// 2. Ekin obýektini düzýäris
 final Ekin mekgejowenYazlyk = Ekin(
   ady: "Mekgejöwen (ýazlyk)",
   tapgyrlar: [
-    Tapgyr(ady: "Sürümden öňki", normaM3: 600, gidroModul: 0.169, baha: 17.88, mohlet: "1.X-10.XI", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 41),
-    Tapgyr(ady: "Tagt suwy", normaM3: 1500, gidroModul: 0.598, baha: 44.70, mohlet: "25.II-25.III", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 29),
-    Tapgyr(ady: "1-nji ösüş", normaM3: 1000, gidroModul: 0.643, baha: 29.80, mohlet: "21.IV-8.V", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 18),
-    Tapgyr(ady: "2-nji ösüş", normaM3: 1000, gidroModul: 0.723, baha: 29.80, mohlet: "9.V-24.V", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 16),
-    Tapgyr(ady: "3-nji ösüş", normaM3: 1100, gidroModul: 0.796, baha: 32.78, mohlet: "25.V-9.VI", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 16),
-    Tapgyr(ady: "4-nji ösüş", normaM3: 1200, gidroModul: 0.992, baha: 35.76, mohlet: "10.VI-23.VI", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 14),
-    Tapgyr(ady: "5-nji ösüş", normaM3: 1200, gidroModul: 1.157, baha: 35.76, mohlet: "24.VI-5.VII", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 12),
-    Tapgyr(ady: "6-njy ösüş", normaM3: 1100, gidroModul: 1.060, baha: 32.78, mohlet: "6.VII-17.VII", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 12),
+    Tapgyr(ady: "Sürümden öňki", normaM3: (600 * 1.15).toInt(), gidroModul: 0.169, baha: 17.88, mohlet: "1.X-10.XI", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 41),
+    Tapgyr(ady: "Tagt suwy", normaM3: (1500 * 1.15).toInt(), gidroModul: 0.598, baha: 44.70, mohlet: "25.II-25.III", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 29),
+    Tapgyr(ady: "1-nji ösüş", normaM3: (1000 * 1.15).toInt(), gidroModul: 0.643, baha: 29.80, mohlet: "21.IV-8.V", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 18),
+    Tapgyr(ady: "2-nji ösüş", normaM3: (1000 * 1.15).toInt(), gidroModul: 0.723, baha: 29.80, mohlet: "9.V-24.V", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 16),
+    Tapgyr(ady: "3-nji ösüş", normaM3: (1100 * 1.15).toInt(), gidroModul: 0.796, baha: 32.78, mohlet: "25.V-9.VI", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 16),
+    Tapgyr(ady: "4-nji ösüş", normaM3: (1200 * 1.15).toInt(), gidroModul: 0.992, baha: 35.76, mohlet: "10.VI-23.VI", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 14),
+    Tapgyr(ady: "5-nji ösüş", normaM3: (1200 * 1.15).toInt(), gidroModul: 1.157, baha: 35.76, mohlet: "24.VI-5.VII", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 12),
+    Tapgyr(ady: "6-njy ösüş", normaM3: (1100 * 1.15).toInt(), gidroModul: 1.060, baha: 32.78, mohlet: "6.VII-17.VII", çeşme: "Maglumat ýok", dowamlylykGijeGundiz: 12),
   ],
 );
