deb1fab947dfee4e71a62408221f0ad07554ad8d
Menli <menliasyrowa91@gmail.com>
2026-07-08 12:28:58 +0500

    Update yorunja_yazlyk.dart

--- lib/yorunja_yazlyk.dart
+++ lib/yorunja_yazlyk.dart
@@ -4,12 +4,12 @@ import 'ekin_model.dart';
 final Ekin yorunjaYazlykData = Ekin(
   ady: "Ýorunja (taze ýazda ekilýän)",
   tapgyrlar: [
-    Tapgyr(ady: "Tagt suwy", normaM3: 1600, gidroModul: 0.661, baha: 47.68, mohlet: "01.II-28.II", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 28),
-    Tapgyr(ady: "1-nji ösüş", normaM3: 1000, gidroModul: 0.445, baha: 29.80, mohlet: "21.III-16.V", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 26),
-    Tapgyr(ady: "2-nji ösüş", normaM3: 1000, gidroModul: 0.445, baha: 29.80, mohlet: "17.V-11.VI", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 26),
-    Tapgyr(ady: "3-nji ösüş", normaM3: 1100, gidroModul: 0.490, baha: 32.78, mohlet: "12.VI-07.VII", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 26),
-    Tapgyr(ady: "4-nji ösüş", normaM3: 1200, gidroModul: 0.556, baha: 35.76, mohlet: "08.VII-01.VIII", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 25),
-    Tapgyr(ady: "5-nji ösüş", normaM3: 1200, gidroModul: 0.556, baha: 35.76, mohlet: "02.VIII-26.VIII", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 25),
-    Tapgyr(ady: "6-njy ösüş", normaM3: 1000, gidroModul: 0.445, baha: 29.80, mohlet: "27.VIII-21.IX", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 26),
+    Tapgyr(ady: "Tagt suwy", normaM3: (1600 * 1.15).toInt(), gidroModul: 0.661, baha: 47.68, mohlet: "01.II-28.II", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 28),
+    Tapgyr(ady: "1-nji ösüş", normaM3: (1000 * 1.15).toInt(), gidroModul: 0.445, baha: 29.80, mohlet: "21.III-16.V", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 26),
+    Tapgyr(ady: "2-nji ösüş", normaM3: (1000 * 1.15).toInt(), gidroModul: 0.445, baha: 29.80, mohlet: "17.V-11.VI", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 26),
+    Tapgyr(ady: "3-nji ösüş", normaM3: (1100 * 1.15).toInt(), gidroModul: 0.490, baha: 32.78, mohlet: "12.VI-07.VII", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 26),
+    Tapgyr(ady: "4-nji ösüş", normaM3: (1200 * 1.15).toInt(), gidroModul: 0.556, baha: 35.76, mohlet: "08.VII-01.VIII", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 25),
+    Tapgyr(ady: "5-nji ösüş", normaM3: (1200 * 1.15).toInt(), gidroModul: 0.556, baha: 35.76, mohlet: "02.VIII-26.VIII", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 25),
+    Tapgyr(ady: "6-njy ösüş", normaM3: (1000 * 1.15).toInt(), gidroModul: 0.445, baha: 29.80, mohlet: "27.VIII-21.IX", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 26),
   ],
 );
