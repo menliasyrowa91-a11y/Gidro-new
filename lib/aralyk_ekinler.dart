932062c4942176e2f9684e5f1a68561d9c61ac07
Menli <menliasyrowa91@gmail.com>
2026-07-09 16:45:39 +0500

    Update aralyk_ekinler.dart

--- lib/aralyk_ekinler.dart
+++ lib/aralyk_ekinler.dart
@@ -4,9 +4,9 @@ import 'ekin_model.dart';
 final Ekin aralykEkinlerData = Ekin(
   ady: "Aralyk ekinler (Mäş, noýba, künji, soýa, günebakar, jöwen, süle)",
   tapgyrlar: [
-    Tapgyr(ady: "Ekişden öňki", normaM3: 1800, gidroModul: 1.040, baha: 53.64, mohlet: "10.VII-29.VII", çeşme: "AnyScanner_06_16_2026-02.jpg", dowamlylykGijeGundiz: 20),
-    Tapgyr(ady: "1-nji ösüş", normaM3: 900, gidroModul: 0.740, baha: 26.82, mohlet: "12.VIII-25.VIII", çeşme: "AnyScanner_06_16_2026-02.jpg", dowamlylykGijeGundiz: 14),
-    Tapgyr(ady: "2-nji ösüş", normaM3: 900, gidroModul: 0.740, baha: 26.82, mohlet: "10.IX-23.IX", çeşme: "AnyScanner_06_16_2026-02.jpg", dowamlylykGijeGundiz: 14),
-    Tapgyr(ady: "3-nji ösüş", normaM3: 900, gidroModul: 0.740, baha: 26.82, mohlet: "2.X-15.X", çeşme: "AnyScanner_06_16_2026-02.jpg", dowamlylykGijeGundiz: 14),
+    Tapgyr(ady: "Ekişden öňki", normaM3: (1800 * 1.15).toInt(), gidroModul: 1.040, baha: 53.64, mohlet: "10.VII-29.VII", çeşme: "AnyScanner_06_16_2026-02.jpg", dowamlylykGijeGundiz: 20),
+    Tapgyr(ady: "1-nji ösüş", normaM3: (900 * 1.15).toInt(), gidroModul: 0.740, baha: 26.82, mohlet: "12.VIII-25.VIII", çeşme: "AnyScanner_06_16_2026-02.jpg", dowamlylykGijeGundiz: 14),
+    Tapgyr(ady: "2-nji ösüş", normaM3: (900 * 1.15).toInt(), gidroModul: 0.740, baha: 26.82, mohlet: "10.IX-23.IX", çeşme: "AnyScanner_06_16_2026-02.jpg", dowamlylykGijeGundiz: 14),
+    Tapgyr(ady: "3-nji ösüş", normaM3: (900 * 1.15).toInt(), gidroModul: 0.740, baha: 26.82, mohlet: "2.X-15.X", çeşme: "AnyScanner_06_16_2026-02.jpg", dowamlylykGijeGundiz: 14),
   ],
 );
