d87c89f0745b92f463c6ecf11ae4c921d01bd106
Menli <menliasyrowa91@gmail.com>
2026-07-13 09:42:46 +0500

    Update ekin_model.dart

--- lib/ekin_model.dart
+++ lib/ekin_model.dart
@@ -1,5 +1,3 @@
-// lib/ekin_model.dart
-
 class Tapgyr {
   final String ady;
   final int normaM3;
@@ -24,12 +22,14 @@ class Ekin {
   final String ady;
   final List<Tapgyr> tapgyrlar;
 
-  // Jemi norma we baha awtomatiki hasaplanýar
   int get jemiNorma => tapgyrlar.fold(0, (sum, item) => sum + item.normaM3);
   double get jemiBaha => tapgyrlar.fold(0.0, (sum, item) => sum + item.baha);
 
-  Ekin({
-    required this.ady,
-    required this.tapgyrlar,
-  });
+  Ekin({required this.ady, required this.tapgyrlar});
+
+  // DropdownButton üçin hökman gerek
+  @override
+  bool operator ==(Object other) => other is Ekin && other.ady == ady;
+  @override
+  int get hashCode => ady.hashCode;
 }
