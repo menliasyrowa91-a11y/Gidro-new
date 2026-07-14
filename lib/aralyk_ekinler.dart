7c22d7b6ea9b945e492edbe6d0231b31cd7faba1
Menli <menliasyrowa91@gmail.com>
2026-07-14 10:17:49 +0500

    Update aralyk_ekinler.dart

--- lib/aralyk_ekinler.dart
+++ lib/aralyk_ekinler.dart
@@ -2,11 +2,43 @@
 import 'ekin_model.dart';
 
 final Ekin aralykEkinlerData = Ekin(
-  ady: "Aralyk ekinler (Mash, noyba, kunji, soya, gunebakar, jowen, sule)",
+  ady: "Aralyk ekinler (Maş, noýba, künji, soýa, günebakar, jöwen, süle)",
   tapgyrlar: [
-    Tapgyr(ady: "Ekishden onki", normaM3: (1800 * 1.15).toInt(), gidroModul: 1.040, baha: 53.64, mohlet: "10.VII-29.VII", cesme: "AnyScanner_06_16_2026-02.jpg", dowamlylyk: 20),
-    Tapgyr(ady: "1-nji osush", normaM3: (900 * 1.15).toInt(), gidroModul: 0.740, baha: 26.82, mohlet: "12.VIII-25.VIII", cesme: "AnyScanner_06_16_2026-02.jpg", dowamlylyk: 14),
-    Tapgyr(ady: "2-nji osush", normaM3: (900 * 1.15).toInt(), gidroModul: 0.740, baha: 26.82, mohlet: "10.IX-23.IX", cesme: "AnyScanner_06_16_2026-02.jpg", dowamlylyk: 14),
-    Tapgyr(ady: "3-nji osush", normaM3: (900 * 1.15).toInt(), gidroModul: 0.740, baha: 26.82, mohlet: "2.X-15.X", cesme: "AnyScanner_06_16_2026-02.jpg", dowamlylyk: 14),
+    Tapgyr(
+      ady: "Ekişden öňki", 
+      normaM3: (1800 * 1.15).toInt(), 
+      gidroModul: 1.040, 
+      baha: 53.64, 
+      mohlet: "10.VII-29.VII", 
+      çeşme: "AnyScanner_06_16_2026-02.jpg", 
+      dowamlylykGijeGundiz: 20
+    ),
+    Tapgyr(
+      ady: "1-nji ösüş", 
+      normaM3: (900 * 1.15).toInt(), 
+      gidroModul: 0.740, 
+      baha: 26.82, 
+      mohlet: "12.VIII-25.VIII", 
+      çeşme: "AnyScanner_06_16_2026-02.jpg", 
+      dowamlylykGijeGundiz: 14
+    ),
+    Tapgyr(
+      ady: "2-nji ösüş", 
+      normaM3: (900 * 1.15).toInt(), 
+      gidroModul: 0.740, 
+      baha: 26.82, 
+      mohlet: "10.IX-23.IX", 
+      çeşme: "AnyScanner_06_16_2026-02.jpg", 
+      dowamlylykGijeGundiz: 14
+    ),
+    Tapgyr(
+      ady: "3-nji ösüş", 
+      normaM3: (900 * 1.15).toInt(), 
+      gidroModul: 0.740, 
+      baha: 26.82, 
+      mohlet: "2.X-15.X", 
+      çeşme: "AnyScanner_06_16_2026-02.jpg", 
+      dowamlylykGijeGundiz: 14
+    ),
   ],
 );
