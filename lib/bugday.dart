c07e1927c7120a5416af070293b329a150549fe4
Menli <menliasyrowa91@gmail.com>
2026-07-14 10:27:21 +0500

    Update bugday.dart

--- lib/bugday.dart
+++ lib/bugday.dart
@@ -1,44 +1,44 @@
-// lib/bugday.dart
+// ignore_for_file: illegal_character
 import 'ekin_model.dart';
 
 final Ekin bugdayData = Ekin(
-  ady: "Bugday",
+  ady: "Bugdaý",
   tapgyrlar: [
     Tapgyr(
-      ady: "Tagt ya-da gogerish",
+      ady: "Tagt ýa-da gögeriş",
       normaM3: (1500 * 1.15).toInt(), 
       gidroModul: 0.668,
       baha: 0.0298,
       mohlet: "05.IX-30.IX",
-      cesme: "12.02.2024 tassyknamasy",
-      dowamlylyk: 26,
+      çeşme: "12.02.2024 tassyknamasy",
+      dowamlylykGijeGundiz: 26,
     ),
     Tapgyr(
-      ady: "1-nji osush",
+      ady: "1-nji ösüş",
       normaM3: (1300 * 1.15).toInt(),
       gidroModul: 0.274,
       baha: 0.0298,
       mohlet: "01.X-25.XII",
-      cesme: "12.02.2024 tassyknamasy",
-      dowamlylyk: 55,
+      çeşme: "12.02.2024 tassyknamasy",
+      dowamlylykGijeGundiz: 55,
     ),
     Tapgyr(
-      ady: "2-nji osush",
+      ady: "2-nji ösüş",
       normaM3: (1300 * 1.15).toInt(),
       gidroModul: 0.367,
       baha: 0.0298,
       mohlet: "16.II-28.III",
-      cesme: "12.02.2024 tassyknamasy",
-      dowamlylyk: 41,
+      çeşme: "12.02.2024 tassyknamasy",
+      dowamlylykGijeGundiz: 41,
     ),
     Tapgyr(
-      ady: "3-nji osush",
+      ady: "3-nji ösüş",
       normaM3: (1200 * 1.15).toInt(),
       gidroModul: 0.316,
       baha: 0.0298,
       mohlet: "29.III-11.V",
-      cesme: "12.02.2024 tassyknamasy",
-      dowamlylyk: 44,
+      çeşme: "12.02.2024 tassyknamasy",
+      dowamlylykGijeGundiz: 44,
     ),
   ],
 );
