086d8cc94f69e488eeb29fc40fb06c868ab7c393
Menli <menliasyrowa91@gmail.com>
2026-07-15 16:23:40 +0500

    Update ekin_model.dart

--- lib/ekin_model.dart
+++ lib/ekin_model.dart
@@ -8,17 +8,16 @@ class Tapgyr {
   final String cesme; 
   final int dowamlylykGijeGundiz; 
 
-  // Köne atlary hem kabul edýän konstruktor:
   Tapgyr({
     required this.ady,
     required this.normaM3,
     required this.gidroModul,
     required this.baha,
     required this.mohlet,
-    required String cesme, 
-    String? çeşme, // Köne ady ýumşak görnüşde kabul edýär
-    required int dowamlylykGijeGundiz,
-    int? dowamlylyk, // Köne ady ýumşak görnüşde kabul edýär
-  }) : this.cesme = cesme ?? çeşme!, 
-       this.dowamlylykGijeGundiz = dowamlylykGijeGundiz ?? dowamlylyk!;
+    String? cesme,
+    String? çeşme,
+    int? dowamlylykGijeGundiz,
+    int? dowamlylyk,
+  }) : this.cesme = cesme ?? çeşme ?? "...",
+       this.dowamlylykGijeGundiz = dowamlylykGijeGundiz ?? dowamlylyk ?? 0;
 }
