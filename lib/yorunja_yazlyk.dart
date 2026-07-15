c290fd5e17db58c19c7968836a664520b4e7d82f
Menli <menliasyrowa91@gmail.com>
2026-07-15 10:15:58 +0500

    Update yorunja_yazlyk.dart

--- lib/yorunja_yazlyk.dart
+++ lib/yorunja_yazlyk.dart
@@ -3,14 +3,14 @@
 import 'ekin_model.dart';
 
 final Ekin yorunjaYazlykData = Ekin(
-  ady: "Yorunja (taze yazda ekilyan)",
+  ady: "Ýorunja (täze ýazda ekilýän)",
   tapgyrlar: [
-    Tapgyr(ady: "Tagt uwy", normaM3: (1600 * 1.15).toInt(), gidroModul: 0.661, baha: 47.68, mohlet: "01.III-28.III", cesme: "...", dowamlylyk: 28),
-    Tapgyr(ady: "1-nji osush", normaM3: (1000 * 1.15).toInt(), gidroModul: 0.445, baha: 29.80, mohlet: "21.III-16.V", cesme: "...", dowamlylyk: 26),
-    Tapgyr(ady: "2-nji osush", normaM3: (1000 * 1.15).toInt(), gidroModul: 0.445, baha: 29.80, mohlet: "17.V-11.VI", cesme: "...", dowamlylyk: 26),
-    Tapgyr(ady: "3-nji osush", normaM3: (1100 * 1.15).toInt(), gidroModul: 0.490, baha: 32.78, mohlet: "12.VI-07.VII", cesme: "...", dowamlylyk: 26),
-    Tapgyr(ady: "4-nji osush", normaM3: (1200 * 1.15).toInt(), gidroModul: 0.556, baha: 35.76, mohlet: "08.VII-01.VIII", cesme: "...", dowamlylyk: 25),
-    Tapgyr(ady: "5-nji osush", normaM3: (1200 * 1.15).toInt(), gidroModul: 0.556, baha: 35.76, mohlet: "02.VIII-26.VIII", cesme: "...", dowamlylyk: 25),
-    Tapgyr(ady: "6-njy osush", normaM3: (1000 * 1.15).toInt(), gidroModul: 0.445, baha: 29.80, mohlet: "27.VIII-21.IX", cesme: "...", dowamlylyk: 26),
+    Tapgyr(ady: "Tagt suwy", normaM3: (1600 * 1.15).toInt(), gidroModul: 0.661, baha: 47.68, mohlet: "01.III-28.III", cesme: "...", dowamlylykGijeGundiz: 28),
+    Tapgyr(ady: "1-nji ösüş", normaM3: (1000 * 1.15).toInt(), gidroModul: 0.445, baha: 29.80, mohlet: "21.III-16.V", cesme: "...", dowamlylykGijeGundiz: 26),
+    Tapgyr(ady: "2-nji ösüş", normaM3: (1000 * 1.15).toInt(), gidroModul: 0.445, baha: 29.80, mohlet: "17.V-11.VI", cesme: "...", dowamlylykGijeGundiz: 26),
+    Tapgyr(ady: "3-nji ösüş", normaM3: (1100 * 1.15).toInt(), gidroModul: 0.490, baha: 32.78, mohlet: "12.VI-07.VII", cesme: "...", dowamlylykGijeGundiz: 26),
+    Tapgyr(ady: "4-nji ösüş", normaM3: (1200 * 1.15).toInt(), gidroModul: 0.556, baha: 35.76, mohlet: "08.VII-01.VIII", cesme: "...", dowamlylykGijeGundiz: 25),
+    Tapgyr(ady: "5-nji ösüş", normaM3: (1200 * 1.15).toInt(), gidroModul: 0.556, baha: 35.76, mohlet: "02.VIII-26.VIII", cesme: "...", dowamlylykGijeGundiz: 25),
+    Tapgyr(ady: "6-njy ösüş", normaM3: (1000 * 1.15).toInt(), gidroModul: 0.445, baha: 29.80, mohlet: "27.VIII-21.IX", cesme: "...", dowamlylykGijeGundiz: 26),
   ],
 );
