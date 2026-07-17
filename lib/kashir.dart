cec7c3db244704e73773ca9a8cade53faac77495
Menli <menliasyrowa91@gmail.com>
2026-07-17 11:09:07 +0500

    Update kashir.dart

--- lib/kashir.dart
+++ lib/kashir.dart
@@ -1,22 +1,21 @@
-// ignore_for_file: illegal_character
 // lib/kashir.dart
 import 'ekin_model.dart';
 
 final Ekin kashirData = Ekin(
-  ady: "Käşir (Irki ekilýän gök ekin)",
+  ady: "Kashir (Irki ekilyan gok ekin)",
   tapgyrlar: [
-    Tapgyr(ady: "Sürümden öňki", normaM3: 500, gidroModul: 0.214, baha: 14.90, mohlet: "15.X-10.XI", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 27),
-    Tapgyr(ady: "Ekişden öňki", normaM3: 1500, gidroModul: 0.445, baha: 44.70, mohlet: "10.XI-20.III", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 39),
-    Tapgyr(ady: "Gögeriş suwy (2x600)", normaM3: 1200, gidroModul: 0.609, baha: 35.76, mohlet: "25.XI-15.II", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 19),
-    Tapgyr(ady: "1-nji ösüş", normaM3: 600, gidroModul: 0.434, baha: 17.88, mohlet: "16.III-31.III", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 16),
-    Tapgyr(ady: "2-nji ösüş", normaM3: 600, gidroModul: 0.462, baha: 17.88, mohlet: "1.IV-15.IV", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 15),
-    Tapgyr(ady: "3-nji ösüş", normaM3: 600, gidroModul: 0.631, baha: 17.88, mohlet: "16.IV-23.IV", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 11),
-    Tapgyr(ady: "4-nji ösüş", normaM3: 600, gidroModul: 0.694, baha: 17.88, mohlet: "27.IV-6.V", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 10),
-    Tapgyr(ady: "5-nji ösüş", normaM3: 600, gidroModul: 0.771, baha: 17.88, mohlet: "7.V-15.V", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 9),
-    Tapgyr(ady: "6-njy ösüş", normaM3: 600, gidroModul: 0.771, baha: 17.88, mohlet: "16.V-24.V", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 9),
-    Tapgyr(ady: "7-nji ösüş", normaM3: 600, gidroModul: 0.771, baha: 17.88, mohlet: "25.V-2.VI", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 8),
-    Tapgyr(ady: "8-nji ösüş", normaM3: 600, gidroModul: 0.868, baha: 17.88, mohlet: "3.VI-10.VI", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 8),
-    Tapgyr(ady: "9-njy ösüş", normaM3: 600, gidroModul: 0.868, baha: 17.88, mohlet: "11.VI-18.VIII", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 8),
-    Tapgyr(ady: "10-njy ösüş", normaM3: 600, gidroModul: 0.992, baha: 17.88, mohlet: "19.VI-25.VI", çeşme: "AnyScanner_06_16_2026-06.jpg", dowamlylykGijeGundiz: 7),
+    Tapgyr(ady: "Surumden onki", normaM3: 500, gidroModul: 0.214, baha: 14.90, mohlet: "15.X-10.XI", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 27),
+    Tapgyr(ady: "Ekisden onki", normaM3: 1500, gidroModul: 0.445, baha: 44.70, mohlet: "10.XI-20.III", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 39),
+    Tapgyr(ady: "Gogeris suwy (2x600)", normaM3: 1200, gidroModul: 0.609, baha: 35.76, mohlet: "25.XI-15.II", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 19),
+    Tapgyr(ady: "1-nji osus", normaM3: 600, gidroModul: 0.434, baha: 17.88, mohlet: "16.III-31.III", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 16),
+    Tapgyr(ady: "2-nji osus", normaM3: 600, gidroModul: 0.462, baha: 17.88, mohlet: "1.IV-15.IV", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 15),
+    Tapgyr(ady: "3-nji osus", normaM3: 600, gidroModul: 0.631, baha: 17.88, mohlet: "16.IV-23.IV", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 11),
+    Tapgyr(ady: "4-nji osus", normaM3: 600, gidroModul: 0.694, baha: 17.88, mohlet: "27.IV-6.V", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 10),
+    Tapgyr(ady: "5-nji osus", normaM3: 600, gidroModul: 0.771, baha: 17.88, mohlet: "7.V-15.V", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 9),
+    Tapgyr(ady: "6-njy osus", normaM3: 600, gidroModul: 0.771, baha: 17.88, mohlet: "16.V-24.V", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 9),
+    Tapgyr(ady: "7-nji osus", normaM3: 600, gidroModul: 0.771, baha: 17.88, mohlet: "25.V-2.VI", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 8),
+    Tapgyr(ady: "8-nji osus", normaM3: 600, gidroModul: 0.868, baha: 17.88, mohlet: "3.VI-10.VI", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 8),
+    Tapgyr(ady: "9-njy osus", normaM3: 600, gidroModul: 0.868, baha: 17.88, mohlet: "11.VI-18.VIII", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 8),
+    Tapgyr(ady: "10-njy osus", normaM3: 600, gidroModul: 0.992, baha: 17.88, mohlet: "19.VI-25.VI", cesme: "AnyScanner.jpg", dowamlylykGijeGundiz: 7),
   ],
 );
